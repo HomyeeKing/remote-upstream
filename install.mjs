86012fd99e0d3578838fe40204ce8d7a6b82a28a
HomyeeKing <HomyeeKing@gmail.com>
2022-02-16 22:02:18 +0800

    feat: use references

--- install.mjs
+++ install.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-import "zx/globals";
+/// <reference path="node_modules/zx/globals.d.ts" />
 
 const upstreamCommand =
   "curl -o- https://raw.githubusercontent.com/HomyeeKing/remote-upstream/master/upstream.mjs | zx";
