2bbf56117bcb9e88899fbb9ee350ccadb9a13b3c
HomyeeKing <HomyeeKing@gmail.com>
2022-02-16 20:18:42 +0800

    chore: one

---
+++ upstream.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env zx
+
+const origin = (await $`git remote get-url  origin`).stdout.trim()
+
+if (origin.startsWith("git@")) {
+  const reg = /^git@github\.com:(.*)\.git$/
+  const userAndRepoMatch = origin.match(reg)
+  if (userAndRepoMatch) {
+    const userAndRepo = userAndRepoMatch[1].trim()
+    const res = await fetch(`https://api.github.com/repos/${userAndRepo}`)
+    if (res.ok) {
+      const info = await res.json()
+      if (info.fork) {
+        await $`git remote add upstream ${info.parent.ssh_url}`
+        console.log(
+          chalk.green(`ADD UPSTREAM SUCCESSFULLY. CHECK THE GIT REMOTE`),
+        )
+        await $`git remote -v`
+      }
+    } else {
+      console.error(`Failed to get the ${userAndRepo} info,  please file an issue: 
+       https://github.com/HomyeeKing/remote-upstream/issues`)
+    }
+  } else {
+    console.error(
+      chalk.red(`[ERROR!] Failed to address the repo info, please file an issue to make this fixed. 
+    https://github.com/HomyeeKing/remote-upstream/issues`),
+    )
+  }
+} else {
+  console.warn(`Can't find origin remote`)
+}
