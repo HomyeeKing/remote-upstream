2f645a743628006c2f08388d7c2862c3b9777093
HomyeeKing <HomyeeKing@gmail.com>
2022-02-23 10:19:41 +0800

    feat: download file

--- install.mjs
+++ install.mjs
@@ -1,9 +1,8 @@
 #!/usr/bin/env zx
 /// <reference types="zx/globals" />
-$.verbose = false
+// $.verbose = false
 
-const upstreamCommand =
-  "zx https://raw.githubusercontent.com/HomyeeKing/remote-upstream/main/get-upstream.mjs";
+const upstream_file_name = 'get-upstream.mjs'
 
 // git init template reference https://git-scm.com/docs/git-init#_template_directory
 const git_init_template =
@@ -14,16 +13,11 @@ const git_init_template =
 const hooksPath = path.join(git_init_template, "./hooks");
 const postCheckout = path.join(hooksPath, "post-checkout");
 
-if (fs.existsSync(postCheckout)) {
-  const content = fs.readFileSync(postCheckout, "utf-8");
-  if (content.includes(upstreamCommand)) {
-    console.log(chalk.yellow`upstream command has been injected`);
-  } else {
-    fs.writeFileSync(postCheckout, `${content}\n${upstreamCommand}`);
-    console.log(chalk.green`upstream command is injected successfully`);
-  }
-} else {
+if (!fs.existsSync(postCheckout)) {
   console.log(chalk.red(`${postCheckout} is not exist, will create it for you`));
-  await $`echo ${upstreamCommand} > post-checkout`
 }
 
+cd(hooksPath)
+await $`curl -O https://raw.githubusercontent.com/HomyeeKing/remote-upstream/main/${upstream_file_name}`
+await $`chmod +x ./${upstream_file_name}`
+await $`echo ./${upstream_file_name} >> ${postCheckout}`
